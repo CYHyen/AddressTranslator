232574915be7ab7667035cf3c860e07958b99533
Huang Chung Yen <justin.huang@mobile.com.tw>
2021-03-09 10:47:58 +0800

    加入專案檔案。

---
+++ AddressTranslator/AddressRegex.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AddressTranslator
+{
+    public class AddressRegex
+    {
+
+        public AddressRegex(string address)
+        {
+            this.OrginalAddress = address;
+            this.ParseByRegex(address);
+        }
+        /// <summary>
+        /// 縣市、包含一級行政區&二級行政區
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// 鄉鎮市區 三級行政區
+        /// </summary>
+        public string District { get; set; }
+
+        /// <summary>
+        /// 村里
+        /// </summary>
+        public string Village { get; set; }
+
+        /// <summary>
+        /// 鄰
+        /// </summary>
+        public string Neighborhood { get; set; }
+
+        /// <summary>
+        /// 路街段道
+        /// </summary>
+        public string Street { get; set; }
+
+        /// <summary>
+        ///巷 
+        /// </summary>
+        public string Lane { get; set; }
+
+        /// <summary>
+        /// 弄
+        /// </summary>
+        public string Alley { get; set; }
+        /// <summary>
+        /// 號
+        /// </summary>
+        public string Number { get; set; }
+
+        /// <summary>
+        /// 樓
+        /// </summary>
+        public string Floor { get; set; }
+
+        /// <summary>
+        /// 剩下的地址
+        /// </summary>
+        public string Others { get; set; }
+        /// <summary>
+        /// 是否符合pattern規範
+        /// </summary>
+        public bool IsParseSuccessed { get; set; }
+        /// <summary>
+        /// 原始傳入的地址
+        /// </summary>
+        public string OrginalAddress { get; private set; }
+
+
+
+        private void ParseByRegex(string address)
+        {
+            string strict_pattern = @"(?<City>(\S{2}[縣市]))(?<District>(\S{2}[鄉鎮市區]))(?<Village>\S+[村里])?(?<Neighborhood>\S+[鄰])?(?<Street>\S+[路道街段])?(?<Lane>\S+[巷])?(?<Alley>\S+[弄])?(?<Number>\S+[號])?(?<Floor>\S+[樓])?(?<Others>.+)?";
+            string pattern = @"(?<City>\S+[縣市])(?<District>\S+?(市區|鎮區|鎮市|[鄉鎮市區]))(?<Village>\S+[村里])?(?<Neighborhood>\S+[鄰])?(?<Street>\S+[路道街段])?(?<Lane>\S+[巷])?(?<Alley>\S+[弄])?(?<Number>\S+[號])?(?<Floor>\S+[樓])?(?<Others>.+)?";
+            Match match = Regex.Match(address, strict_pattern);
+
+            if (match.Success)
+            {
+                this.IsParseSuccessed = true;
+                this.City = match.Groups["City"].ToString();
+                this.District = match.Groups["District"].ToString();
+                this.Village = match.Groups["Village"].ToString();
+                this.Neighborhood = match.Groups["Neighborhood"].ToString();
+                this.Street = match.Groups["Street"].ToString();
+                this.Lane = match.Groups["Lane"].ToString();
+                this.Alley = match.Groups["Alley"].ToString();
+                this.Number = match.Groups["Number"].ToString();
+                this.Floor = match.Groups["Floor"].ToString();
+                this.Others = match.Groups["Others"].ToString();
+                return;
+
+            }
+            match = Regex.Match(address, pattern);
+            if (match.Success)
+            {
+                this.IsParseSuccessed = true;
+                this.City = match.Groups["City"].ToString();
+                this.District = match.Groups["District"].ToString();
+                this.Village = match.Groups["Village"].ToString();
+                this.Neighborhood = match.Groups["Neighborhood"].ToString();
+                this.Street = match.Groups["Street"].ToString();
+                this.Lane = match.Groups["Lane"].ToString();
+                this.Alley = match.Groups["Alley"].ToString();
+                this.Number = match.Groups["Number"].ToString();
+                this.Floor = match.Groups["Floor"].ToString();
+                this.Others = match.Groups["Others"].ToString();
+                return;
+
+            }
+
+
+        }
+    }
+
+
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using AddressTranslator;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //台北市南港區三重路66號14樓
+            string address = @"台中市東勢區本街&本街1巷";
+            AddressRegex target = new AddressRegex(address);
+            Console.WriteLine(target.OrginalAddress);
+            Console.WriteLine("市:" + target.City);
+            Console.WriteLine("區:" + target.District);
+            Console.WriteLine("里:" + target.Village);
+            Console.WriteLine("鄰:" + target.Neighborhood);
+            Console.WriteLine("路:" + target.Street);
+            Console.WriteLine("巷:" + target.Lane);
+            Console.WriteLine("弄:" + target.Alley);
+            Console.WriteLine("號:" + target.Number);
+            Console.WriteLine("樓:" + target.Floor);
+
+            string myaddress = @"台北市中正區羅斯福路四段136巷6弄6-1號";
+            AddressRegex _target = new AddressRegex(myaddress);
+            Console.WriteLine(_target.OrginalAddress);
+            Console.WriteLine("市:" + _target.City);
+            Console.WriteLine("區:" + _target.District);
+            Console.WriteLine("里:" + _target.Village);
+            Console.WriteLine("鄰:" + _target.Neighborhood);
+            Console.WriteLine("路:" + _target.Street);
+            Console.WriteLine("巷:" + _target.Lane);
+            Console.WriteLine("弄:" + _target.Alley);
+            Console.WriteLine("號:" + _target.Number);
+            Console.WriteLine("樓:" + _target.Floor);
+
+            //宜蘭縣蘇澳鎮區界路
+            string _myaddress = @"宜蘭縣蘇澳鎮區界路6-1號";
+            AddressRegex __target = new AddressRegex(_myaddress);
+            Console.WriteLine(__target.OrginalAddress);
+            Console.WriteLine("市:" + __target.City);
+            Console.WriteLine("區:" + __target.District);
+            Console.WriteLine("里:" + __target.Village);
+            Console.WriteLine("鄰:" + __target.Neighborhood);
+            Console.WriteLine("路:" + __target.Street);
+            Console.WriteLine("巷:" + __target.Lane);
+            Console.WriteLine("弄:" + __target.Alley);
+            Console.WriteLine("號:" + __target.Number);
+            Console.WriteLine("樓:" + __target.Floor);
+
+        }
+    }
+
+}
